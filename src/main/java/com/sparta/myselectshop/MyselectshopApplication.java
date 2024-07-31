7560160d914cb331de5d94b98712990842325fab
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-07-31 14:29:29 +0900

    Feat : 관심 상품 API 구현
    관심 상품 저장하는 API 구현
    entity, dto, controller, service, repository 구현 완료

--- src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
+++ src/main/java/com/sparta/myselectshop/MyselectshopApplication.java
@@ -2,12 +2,14 @@ package com.sparta.myselectshop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class MyselectshopApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MyselectshopApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MyselectshopApplication.class, args);
+    }
 
 }
---
+++ src/main/java/com/sparta/myselectshop/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.sparta.myselectshop.controller;
+
+import com.sparta.myselectshop.dto.ProductRequestDto;
+import com.sparta.myselectshop.dto.ProductResponseDto;
+import com.sparta.myselectshop.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api")
+public class ProductController {
+
+    private final ProductService productService;
+
+    // 관심 상품 등록하기
+    @PostMapping("/products")
+    public ProductResponseDto createProduct(@RequestBody ProductRequestDto requestDto) {
+        // 응답 보내기
+        return productService.createProduct(requestDto);
+    }
+}

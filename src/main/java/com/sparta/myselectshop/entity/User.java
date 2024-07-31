ec07cc83cab5bad68484fc79d327b1f6eefc12ab
CJ-1998 <choijun0627@konkuk.ac.kr>
2024-07-31 22:49:06 +0900

    Feat : 회원 가입 및 로그인 기능 구현
    회원 가입 기능 구현 완료
    JWT 및 Spring Security 사용한 로그인 기능 구현 완료

---
+++ src/main/java/com/sparta/myselectshop/entity/User.java
@@ -0,0 +1,44 @@
+package com.sparta.myselectshop.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private UserRoleEnum role;
+
+    public User(String username, String password, String email, UserRoleEnum role) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.role = role;
+    }
+}
---
+++ src/main/java/com/sparta/myselectshop/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.sparta.myselectshop.repository;
+
+import com.sparta.myselectshop.entity.User;
+import java.util.Optional;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUsername(String username);
+
+    Optional<User> findByEmail(String email);
+}
